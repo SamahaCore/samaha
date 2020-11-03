34ce049694b4abcbe7c3cee2f61fdff2f7c9368b
rafeedm <rafeedmohtasim@gmail.com>
2020-11-03 18:46:42 -0500

    coloured underlines

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -3,7 +3,7 @@
     class="card mx-10 my-5 p-5 bg-white rounded-3xl w-2/3 grid place-items-center shadow-lg"
   >
     <img :src="require('@/assets/' + img + '')" alt="logo" />
-    <h1 class="my-4 text-center font-medium font-raleway text-xl underline">
+    <h1 class="title my-4 text-center font-medium font-raleway text-xl">
       {{ title }}
     </h1>
     <p class="mb-4 text-center font-lato text-lg">
@@ -32,5 +32,9 @@ export default {
 <style scoped>
 .card {
   height: 20rem;
+}
+.title {
+  border-bottom: 1px solid #48beff;
+  padding-bottom: 5px;
 }
 </style>
