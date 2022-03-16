e9d5c81253379ae89f9313460437aa4d2950ed3b
Razi <syedru@mcmaster.ca>
2022-03-16 16:36:13 -0400

    Add Poppins font and apply to headings

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -3,7 +3,7 @@ import Navigation from "../Components/Header/Navigation";
 
 const Header = () => {
   return (
-    <header className="basis-1/6 flex flex-row justify-between">
+    <header className="basis-1/6 flex flex-row justify-between font-heading text-2xl">
       <Logo />
       <Navigation />
     </header>
