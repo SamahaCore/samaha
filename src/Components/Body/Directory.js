662446e04000f44190d74384fd8b3f5985d64bc5
Razi <syedru@mcmaster.ca>
2022-03-24 17:00:41 -0400

    Make placeholder for directory

--- src/Components/Body/Directory.js
+++ src/Components/Body/Directory.js
@@ -1,7 +1,13 @@
 import Section from "../../UI/Section";
 
 const Directory = () => {
-  return <Section id="directory">Directory</Section>;
+  return (
+    <Section id="directory">
+      <div className="flex justify-evenly items-center py-20 h-full">
+        <div className="basis-3/4 rounded shadow h-full text-center">Directory</div>
+      </div>
+    </Section>
+  );
 };
 
 export default Directory;
